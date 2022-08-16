9a194d97bc2b833544bca4f05eb845c0224346f3
RachaelMuriithi <rachael.mureithi@student.moringaschool.com>
2022-08-16 18:52:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
 
 // Write your code here
+var num1 = 31;
+var num2 = 2;
+var multiply =  num1 * num2;
+
+var random = Math.floor (Math.random() + 1);
+var num3 = 16;
+var num4 = 6;
+var mod = num3 % num4;
+var max = Math.max (4,5,6,8,10,20);
